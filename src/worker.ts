c96bdb2512dd754a566d3c19eb6f422ada51c4e3
ConstasJ <2020212726@qq.com>
2022-07-17 22:44:47 +0800

    feat: make the basic structure of Worker class

--- src/worker.ts
+++ src/worker.ts
@@ -4,12 +4,16 @@ export default class Worker {
     private app: Express;
     private conf: Record<string, unknown>;
 
-    public register(url: string) {}
-
-    public start() {}
-
     public constructor(conf: Record<string, unknown>, app: Express = express()) {
         this.app = app;
         this.conf = conf;
+        app.use(express.json());
+        app.use(express.urlencoded({ extended: true }));
+    }
+
+    public start() {}
+
+    public register(url: string) {
+        this.app.post(url, (req, res, next) => {});
     }
 }
