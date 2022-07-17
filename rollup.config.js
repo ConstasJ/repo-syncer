4eadda94d09d2bc98b1f61f37f20e0d3978e0af0
ConstasJ <2020212726@qq.com>
2022-07-17 23:01:21 +0800

    chore: reconfigure building

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,21 @@
 import { defineConfig } from 'rollup';
 import ts from 'rollup-plugin-ts';
 
-export default defineConfig([
-    {
-        input: 'src/index.ts',
-        output: {
-            format: 'cjs',
-            file: 'lib/index.js'
+export default defineConfig({
+    input: 'src/index.ts',
+    output: [
+        {
+            file: 'lib/index.js',
+            format: 'commonjs'
         },
-        plugins: [
-            ts({
-                tsconfig: 'tsconfig.json'
-            })
-        ]
-    },
-    {
-        input: 'src/index.ts',
-        output: {
-            format: 'esm',
-            file: 'lib/index.mjs'
-        },
-        plugins: [
-            ts({
-                tsconfig: 'tsconfig.json'
-            })
-        ]
-    }
-]);
+        {
+            file: 'lib/index.mjs',
+            format: 'esm'
+        }
+    ],
+    plugins: [
+        ts({
+            tsconfig: 'tsconfig.json'
+        })
+    ]
+});
