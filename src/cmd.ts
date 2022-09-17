38198388c6bc188c28ceac99edc7c12b52aff3c3
ConstasJ <2020212726@qq.com>
2022-09-17 22:33:23 +0800

    feat: add CLI support

--- src/cmd.ts
+++ src/cmd.ts
@@ -1,1 +1,14 @@
 #!/usr/bin/env node
+import { Command } from 'commander';
+import { sync } from './api';
+
+const cmd = new Command();
+
+cmd.argument('path')
+    .argument('source')
+    .argument('target')
+    .action(async (pth, src, trg) => {
+        await sync(pth, src, trg);
+    });
+
+cmd.parse();
