3f4531c837d62cd5890626557ed18ac12d4aae83
ConstasJ <2020212726@qq.com>
2022-09-17 18:09:07 +0800

    chore: edit rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,34 @@
 import { defineConfig } from 'rollup';
 import ts from 'rollup-plugin-ts';
 
-export default defineConfig({
-    input: 'src/index.ts',
-    output: [
-        {
-            file: 'lib/index.js',
-            format: 'commonjs'
-        },
-        {
+export default defineConfig([
+    {
+        input: 'src/index.ts',
+        output: [
+            {
+                file: 'lib/index.js',
+                format: 'commonjs'
+            }
+        ],
+        plugins: [
+            ts({
+                tsconfig: 'tsconfig.json'
+            })
+        ]
+    },
+    {
+        input: 'src/index.ts',
+        output: {
             file: 'lib/index.mjs',
             format: 'esm'
-        }
-    ],
-    plugins: [
-        ts({
-            tsconfig: 'tsconfig.json'
-        })
-    ]
-});
+        },
+        plugins: [
+            ts({
+                tsconfig: {
+                    fileName: 'tsconfig.json',
+                    hook: (resolvedConfig) => ({ ...resolvedConfig, declaration: false })
+                }
+            })
+        ]
+    }
+]);
