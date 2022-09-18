76555189c66d94863a8f8e89b07142837aa23540
ConstasJ <2020212726@qq.com>
2022-09-18 17:53:52 +0800

    feat(api): add API double-side syncing function
    also change the log layout,add platform name

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,7 @@ import * as path from 'path';
 import * as fs from 'fs';
 import { logger } from './log';
 
-async function sync(pth: string, srcp: string, trgp: string) {
+async function sSync(pth: string, srcp: string, trgp: string) {
     async function hasRemote(remote: string) {
         const git = simpleGit(path.resolve(hPath, `./${pth}`));
         const rems = await git.getRemotes();
@@ -18,16 +18,23 @@ async function sync(pth: string, srcp: string, trgp: string) {
     const gc = simpleGit();
     if (!fs.existsSync(path.resolve(hPath, `./${pth}`))) {
         await gc.mirror(`git@${srcp}.com:${pth}`, path.resolve(hPath, `./${pth}`));
-        logger.info('Cloned repo from source platform.');
+        logger.info(`Cloned repo from source platform(${srcp}).`);
     } else {
         const gl = simpleGit(path.resolve(hPath, `./${pth}`));
         await gl.fetch(['--all']);
-        logger.info('Pulled from source platform.');
+        logger.info(`Pulled from source platform(${srcp}).`);
     }
     const git = simpleGit(path.resolve(hPath, `./${pth}`));
     if (!(await hasRemote(trgp))) await git.addRemote(trgp, `git@${trgp}.com:${pth}`);
     await git.push(['--all', `${trgp}`]);
-    logger.info('Pushed to target platform.');
+    logger.info(`Pushed to target platform(${trgp}).`);
 }
 
-export { sync };
+async function sync(pth: string, srcp: string, trgp: string) {
+    logger.info(`Step 1:Sync from ${srcp} to ${trgp}.`);
+    await sSync(pth, srcp, trgp);
+    logger.info(`Step 2:Sync from ${trgp} to ${srcp}.`);
+    await sSync(pth, trgp, srcp);
+}
+
+export { sSync, sync };
